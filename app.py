0d5e4b171a2040f99e4f09a60955db6211e9e7db
Vivan Vatsa <vivanvatsa@gmail.com>
2021-06-12 20:32:10 +0530

    car price bug fixing

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 def Home():
     return render_template('index.html')
 
-
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
